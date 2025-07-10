71c45abf2b2b6bbc88bb1e306a766fca250e2b70
nielshn <niel.shn08@gmail.com>
2025-07-10 14:02:00 +0700

    change api base url

--- config/api.php
+++ config/api.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'base_url' => env('API_BASE_URL', 'http://127.0.0.1:8000/api'),
-    'qr_code' => env('QR_CODE_URL', 'http://127.0.0.1:8000/storage')
+    'base_url' => env('API_BASE_URL', 'http://service-gudang.tsth2.web.id/api'),
+    'qr_code' => env('QR_CODE_URL', 'http://service-gudang.tsth2.web.id/storage')
 ];
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -5,7 +5,7 @@ import "vue3-toastify/dist/index.css";
 import axios from "axios";
 
 // === CONSTANTS ===
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "http://service-gudang.tsth2.web.id/api";
 
 // === STATE ===
 const show = ref(false);
